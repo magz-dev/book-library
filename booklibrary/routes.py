06b4097faf45cbf89330dbf81a0c95e2d89cc871
magzdev <magdakloskowicz@gmail.com>
2023-04-27 22:40:37 +0000

    add a heading on home page

--- booklibrary/routes.py
+++ booklibrary/routes.py
@@ -3,6 +3,17 @@ from booklibrary import app, db
 from booklibrary.models import Category, Book
 
 
+
 @app.route("/")
 def home():
-    return render_template("books.html")
+    return render_template("home.html")
+
+    
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template('add_category.html')
