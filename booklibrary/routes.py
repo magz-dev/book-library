aacc767e82016041fb6026796ab62783cbcc25f0
magzdev <magdakloskowicz@gmail.com>
2023-07-27 13:45:14 +0000

    In routes.py add a new function called add_book.

--- booklibrary/routes.py
+++ booklibrary/routes.py
@@ -34,13 +34,27 @@ def edit_category(category_id):
     return render_template("edit_category.html", category=category)
 
 
-@app.route("/delete_category")
-def delete_category():
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
 
 
-
+@app.route("/add_book", methods=["GET", "POST"])
+def add_book():
+    categories = list(Category.query.order_by(Category.category_name).all())
+    if request.method == "POST":
+        book = Book(
+            book_title=requrst.form.get("book_title"),
+            book_author=request.form.get("book_author"),
+            current_status=request.form.get("current_status"),
+            category_id=request.form.get("category_id")
+        )
+        db.session.add(book)
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template('add_book.html', categories=categories)
     
