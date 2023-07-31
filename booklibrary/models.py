8fe5562587503f503914112c2017fb61db3748fc
magzdev <magdakloskowicz@gmail.com>
2023-07-31 23:03:38 +0000

    Add a new model in models.py called 'book_description'

--- booklibrary/models.py
+++ booklibrary/models.py
@@ -2,22 +2,26 @@ from booklibrary import db
 
 
 class Category(db.Model):
+    # schema for the category model
     id = db.Column(db.Integer, primary_key=True)
     category_name = db.Column(db.String(20), unique=True, nullable=False)
     books = db.relationship("Book", backref="category", cascade="all, delete", lazy=True)
  
     def __repr__(self):
-        # represent itself in the form of a string
+    # represent itself in the form of a string
         return self.category_name
 
 
 class Book(db.Model):
+    # schema for the book model
     id = db.Column(db.Integer, primary_key=True)
     book_title = db.Column(db.String(50), unique=True, nullable=False)
     book_author = db.Column(db.String(30), nullable=False)
     current_status = db.Column(db.Boolean, nullable=False)
+    book_description = db.Column(db.Text, nullable=False)
     category_id = db.Column(db.Integer, db.ForeignKey("category.id", ondelete="CASCADE"), nullable=False)
+    book_description = db.Column(db.Text, nullable=False)
 
     def __repr__(self):
-        # represent itself in the form of a string
-        return f"#{self.id} - Book:{self.book_title}|Author:{self.book_author}|Status:{self.current_status}"
+    # represent itself in the form of a string
+        return f"#{self.id} - Book:{self.book_title} | Author:{self.book_author} | Status:{self.current_status}"
